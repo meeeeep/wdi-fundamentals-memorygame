e44b01bf65bafea1c589bda55378e13f12e356e5
Stephanie Cray <stephaniecray@Stephanies-MacBook-Pro.local>
2016-10-28 15:16:30 -0400

    added conditionals to JS

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,18 @@ var cardOne= "queen";
 var cardTwo= "queen";
 var cardThree= "king";
 var cardFour= "king";
+
+if (cardOne === cardTwo) {
+alert("You've found a match!");
+}
+else if (cardOne === !cardTwo) {
+alert(" Sorry, try again.");
+}
+
+if (cardThree === cardFour) {
+	alert("You've found a match!");
+}	
+
+else if (cardThree === !cardFour) {
+	alert("Sorry, try again.");
+}
